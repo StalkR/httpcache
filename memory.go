872fcacd4317c0de8ca237117be5347344e66dbd
Dvir Volk <dvir@everything.me>
2014-01-23 12:13:57 +0200

    wrapped cache execution with a mutex

--- memory.go
+++ memory.go
@@ -2,8 +2,10 @@ package httpcache
 
 import (
 	"errors"
+	//"fmt"
 	"github.com/golang/groupcache/lru"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -11,26 +13,35 @@ import (
 // It is not safe for concurrency.
 type memoryCache struct {
 	Cache *lru.Cache
+	mutex *sync.Mutex
 }
 
 func NewMemoryCache(maxItems int) *memoryCache {
 
 	return &memoryCache{
 		Cache: lru.New(maxItems),
+		mutex: &sync.Mutex{},
 	}
 }
 
 // Get gets data saved for an URL if present in cache.
 func (m memoryCache) Get(u *url.URL) (*entry, error) {
+
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
 	data, ok := m.Cache.Get(u.String())
 	if !ok {
 		return nil, errors.New("not in cache")
 	}
+
 	return data.(*entry), nil
 }
 
 // Put puts data of an URL in cache.
 func (m memoryCache) Put(u *url.URL, data []byte) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.Cache.Add(u.String(), &entry{data, time.Now()})
 	return nil
 }
